1c1bd8b042e707bf1e447e09e1178f91e26b3b96
Renat Nasyrov <renat.1998.ru@gmail.com>
2021-11-19 00:41:18 +0300

    дополнил роутер

--- backend/controllers/todoControllers.js
+++ backend/controllers/todoControllers.js
@@ -1,14 +1,54 @@
+const taskModel = require("../models/todoSchema");
+
 const getAllTask = (req, res) => {
-  res.json({ message: "get all task" });
+  taskModel.find((err, tasks) => {
+    if (err) {
+      return console.error(err);
+    }
+    res.json(tasks);
+  });
 };
 
-const getTask = (req, res) => {};
+const getTask = (req, res) => {
+  const id = req.params.id;
+  taskModel.findOne({ _id: id }, (err, task) => {
+    if (err) {
+      return console.error(err);
+    }
+    res.json(task);
+  });
+};
 
-const createNewTask = (req, res) => {};
+const createNewTask = (req, res) => {
+  if (!req.body) {
+    return res.sendStatus(400);
+  }
+//TODO написать парсер json 
+  const task = new taskModel({});
+  task.save();
+};
 
-const deleteTask = (req, res) => {};
+const deleteTask = (req, res) => {
+  const id = req.params.id;
+  taskModel.findByIdAndDelete(id, (err, task) => {
+    if (err) {
+      console.error(err);
+    }
+    res.json(task);
+  });
+};
 
-const updateTask = (req, res) => {};
+const updateTask = (req, res) => {
+  if (!req.body) {
+    return res.sendStatus(400);
+  }
+  const id = req.params.id;
+  taskModel.findByIdAndUpdate(id, req.body, (err, task) => {
+    if (err) {
+      console.error(err);
+    }
+  });
+};
 
 const updatePositionTask = (req, res) => {};
 
