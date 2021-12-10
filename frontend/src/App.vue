68ad3530bd0f603977e09fbab49a7700a3c54a3e
Renat Nasyrov <renat.1998.ru@gmail.com>
2021-12-10 23:52:44 +0300

    feat:added network connection event handling

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -2,14 +2,14 @@
   <v-app>
     <v-main>
       <ErrorBoundary>
-      <ToDo/>
+        <ToDo />
       </ErrorBoundary>
     </v-main>
   </v-app>
 </template>
 
 <script>
-import ErrorBoundary from './components/ErrorBoundary.vue';
+import ErrorBoundary from "./components/ErrorBoundary.vue";
 import ToDo from "./views/ToDoViews.vue";
 
 export default {
@@ -21,7 +21,23 @@ export default {
   },
 
   data: () => ({
-    //
+    onLine: navigator.onLine,
   }),
+  methods: {
+    isOnLine(new_value) {
+      if (!new_value) {
+        this.$toast.info("Упс...Интернет соединение потеряно");
+      } else {
+        this.$toast.info("Интернет соединение восстановлено");
+      }
+    },
+  },
+  watch: {
+    onLine: 'isOnLine'
+  },
+  mounted() {
+    window.addEventListener("online", () => (this.onLine = true));
+    window.addEventListener("offline", () => (this.onLine = false));
+  },
 };
 </script>
